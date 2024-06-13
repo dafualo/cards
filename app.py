5d59a63dcce7ad4a1eacc6e21948c82e530a23d5
Damon <dafualo@gmail.com>
2024-06-13 14:33:17 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import requests as r
+from PIL import Image
+from io import BytesIO
+
+response = r.get('https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+#print(response.status_code)
+deck = response.json()
+print(deck)
+deck_id = deck['deck_id']
+
+response = r.get(f'https://www.deckofcardsapi.com/api/deck/{deck_id}/draw/?count=1')
+card = response.json()
+print(card)
+
+card_image_url = card['cards'][0]['image']
+response = r.get(card_image_url)
+img = Image.open(BytesIO(response.content))
+img.show()
